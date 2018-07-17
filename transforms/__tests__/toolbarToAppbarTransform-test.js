45f5a3f10ce745ffc6cf66a816360c04c8a47362
Dawid <dawidu@onet.pl>
2018-07-17 20:16:31 +0200

    refactor: improves test and add test helper.

--- transforms/__tests__/toolbarToAppbarTransform-test.js
+++ transforms/__tests__/toolbarToAppbarTransform-test.js
@@ -1,24 +1,9 @@
-const fs = require('fs');
-const p = require('path');
-const jscodeshift = require('jscodeshift');
+const testHelper = require('../testHelper');
 
-const read = fileName =>
-  fs.readFileSync(
-    p.join(__dirname, global.baseDir, '__testfixtures__', fileName),
-    'utf8'
+test('toolbar to appbar transform test', () => {
+  const { expected, actual } = testHelper(
+    'toolbarToAppbarTransform',
+    'toolbarToAppbarTransform'
   );
-
-test('toolbarToAppbarTransform', () => {
-  const source = read(`toolbarToAppbarTransform.js`);
-  const output = read(`toolbarToAppbarTransform.output.js`);
-  let transform = require(p.join(global.baseDir, 'toolbarToAppbarTransform'));
-
-  if (transform.default) {
-    transform = transform.default;
-  }
-
-  const ast = jscodeshift(source);
-  const transformedAst = transform(ast, jscodeshift);
-
-  expect(transformedAst.toSource().trim()).toEqual(output.trim());
+  expect(expected).toEqual(actual);
 });
